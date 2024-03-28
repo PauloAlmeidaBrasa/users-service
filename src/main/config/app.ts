8beaa12a169df57b6550f8699198a6403bfed867
pauloprogg <almeida.desenvolve@gmail.com>
2024-03-28 09:00:23 -0300

    feat: add middlewares schemas

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,8 @@
 import express from 'express'
 import setupRoutes from './routes'
+import setupMiddlewares from './middlewares'
 
 const app = express()
 setupRoutes(app)
-
+setupMiddlewares(app)
 export default app
