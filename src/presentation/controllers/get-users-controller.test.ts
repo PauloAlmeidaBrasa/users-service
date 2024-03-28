edb15d85065c621f78b1a20b1cd4c878a1f406af
pauloprogg <almeida.desenvolve@gmail.com>
2024-03-28 13:51:25 -0300

    test: add test in get user controller

---
+++ src/presentation/controllers/get-users-controller.test.ts
@@ -0,0 +1,11 @@
+//import { GetUsersConroller } from "./get-users-controller"
+
+describe('GetuserController test ', () => {
+  test('Should return 500 code error if param miss', () => {
+    // const controller = new GetUsersConroller()
+    // const payload = {
+    //   body: {
+    //   }
+    // }
+  })
+})
